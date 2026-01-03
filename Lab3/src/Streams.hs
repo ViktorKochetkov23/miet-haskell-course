73c8da59c1e1f92a6e6f52cdfaf8c9f6ca175c8c
Виктор Кочетков <viktorkochetkov2305@yandex.ru>
2026-01-04 00:13:52 +0300

    Lab3 - streams functions

--- Lab3/src/Streams.hs
+++ Lab3/src/Streams.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE BangPatterns #-}
 module Streams where
 
 import Data.List(intercalate)
@@ -17,12 +18,13 @@ instance Show a => Show (Stream a) where
 
 -- Реализуйте функцию, превращающую поток в (бесконечный) список
 streamToList :: Stream a -> [a]
-streamToList = undefined
+streamToList (x :> stream) = x : streamToList stream
 
 -- функция, возвращающая n первых элементов потока
 -- удобна для написания тестов следующих функций
 sTake :: Int -> Stream a -> [a]
-sTake = undefined
+sTake 0 _ = []
+sTake n (x :> stream) = x : sTake (n - 1) stream
 
 -- Задание 2 -----------------------------------------
 
@@ -31,7 +33,7 @@ sTake = undefined
 
 -- поток, состоящий из одинаковых элементов
 sRepeat :: a -> Stream a
-sRepeat = undefined
+sRepeat x = x :> sRepeat x
 
 -- sRepeat 1 == [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ...
 
@@ -40,19 +42,20 @@ sRepeat = undefined
 -- будет циклическим (ссылаться сам на себя), а не бесконечно растущим)
 -- sCycle [1, 2, 3] == [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, ...
 sCycle :: [a] -> Stream a
-sCycle = undefined
+sCycle [] = error "Empty stream"
+sCycle xs = (foldr (.) id (map (:>) xs)) (sCycle xs)
 
 -- поток, заданный начальным значением и функцией, строящей следующее значение
 -- по текущему
 -- sIterate (/ 2) 1.0 == [1.0, 0.5, 0.25, 0.125, 0.0625, ...
 sIterate :: (a -> a) -> a -> Stream a
-sIterate = undefined
+sIterate f x = x :> sIterate f (f x)
 
 -- функция, возвращающая поток из чередующихся элементов двух потоков
 -- (для следующего задания нужно сделать эту функцию ленивой по
 -- второму аргументу, то есть не сопоставлять его с образцом)
 sInterleave :: Stream a -> Stream a -> Stream a
-sInterleave (_ :> _) _ = undefined
+sInterleave (x :> stream1) stream2 = x :> sInterleave stream2 stream1
 
 -- sInterleave (sRepeat 1) (sRepeat 2) == [1, 2, 1, 2, 1, 2, ...
 
@@ -62,7 +65,7 @@ sInterleave (_ :> _) _ = undefined
 
 -- поток натуральных чисел (начиная с 0)
 nats :: Stream Integer
-nats = undefined
+nats = sIterate (+1) 0
 
 -- nats == [0, 1, 2, 3, 4, 5, 6, 7, ...
 
@@ -70,8 +73,10 @@ nats = undefined
 -- делящая n нацело. Подсказка: с помощью sInterleave это можно сделать без
 -- проверок на делимость, если её реализация ленива по второму аргументу
 -- (подумайте, почему это важно).
+powersOf2 :: Integer -> Stream Integer
+powersOf2 x = sInterleave (sRepeat x) (powersOf2 (x + 1))
 ruler :: Stream Integer
-ruler = undefined
+ruler = sInterleave (sRepeat 0) (powersOf2 1)
 
 -- ruler == [0, 1, 0, 2, 0, 1, 0, 3, ...
 
@@ -90,14 +95,27 @@ minMaxSlow xs = Just (minimum xs, maximum xs)
 
 {- -O0: Total time: ??? Total Memory in use: ??? -}
 {- -O2: Total time: ??? Total Memory in use: ??? -}
-minMax = undefined
+modifyMinMaxPair :: Ord a => Maybe (a, a) -> a -> Maybe (a, a)
+modifyMinMaxPair Nothing x = Just (x, x)
+modifyMinMaxPair (Just (min, max)) x = Just (if min > x then x else min, if max < x then x else max)
+
+minMax [] = Nothing
+minMax (x: xs) = case minMax xs of
+    Nothing -> Just (x, x)
+    Just (min, max) -> Just (if min > x then x else min, if max < x then x else max)
+
 
 -- Дополнительное задание: реализуйте ту же самую функцию (под названием minMaxBang) с
 -- использованием явной строгости (seq и/или !)
 
 {- -O0: Total time: ??? Total Memory in use: ??? -}
 {- -O2: Total time: ??? Total Memory in use: ??? -}
-minMaxBang = undefined
+minMaxBang [] = Nothing
+minMaxBang (x: xs) =
+      let !rest = minMaxBang xs
+      in case rest of
+        Nothing -> Just (x, x)
+        Just (min, max) -> Just (if min > x then x else min, if max < x then x else max)
 
 -- Скомпилируйте программу с аргументами `ghc Streams.hs -O2 -rtsopts -main-is Streams`
 -- и запустите `Streams.exe +RTS -s` (`./Streams +RTS -s` в Linux/OSX).
